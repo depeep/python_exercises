3a08a619d599ff6bce746c6ede58f716e52ce3d4
depeep <36601963+depeep@users.noreply.github.com>
2026-02-25 16:33:19 +0100

    bijgewerkt

---
+++ genetica.py
@@ -0,0 +1,7 @@
+genoVader = input ("geef het genotype van vader")
+genoMoeder = input ("geef het genotype van moeder")
+dominantA = input ("geef het fenotype dat bij de dominante eigenschap A hoort")
+recessiefA = input ("geef het fenotype dat bij de recessieve eigenschap a hoort")
+
+
+                
