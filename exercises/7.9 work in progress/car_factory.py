f4eab8db193873958923c2609495742d71f79cf5
josvp <josvp@live.nl>
2026-03-27 11:10:19 +0100

    Changes to be committed: 	modified:   exercises/7.9 work in progress/__pycache__/car_factory.cpython-313.pyc 	modified:   exercises/7.9 work in progress/car_factory.py
    Changes not staged for commit:
    	deleted:    exercises/7.9 work in progress/__pycache__/car.cpython-313.pyc
    	deleted:    exercises/7.9 work in progress/__pycache__/car_factory.cpython-313.pyc
    	deleted:    exercises/7.9 work in progress/car.py
    	deleted:    exercises/7.9 work in progress/car_factory.py
    	deleted:    exercises/7.9 work in progress/program.py
    
    Untracked files:
    	exercises/7.9/

--- exercises/7.9 work in progress/car_factory.py
+++ exercises/7.9 work in progress/car_factory.py
@@ -8,19 +8,40 @@ class CarFactory:
         self.__inventory = inventory
         
 # import random
-    def produce_cars(nr_to_produce):
+    def produce_cars(self, nr_to_produce):
         available_models =['Ford', 'Maseratti', 'Renault', 'Tesla', 'Volkswagen', 'Volvo']
         available_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+        list = []
         # print ('The following cars were produced')
         for i in range (nr_to_produce):
             model = choice(available_models)
             color = choice(available_colors)
             carToAdd = Car(model, color)
-            return carToAdd
+            self.__inventory.append(carToAdd)
+            list.append(carToAdd)
+        return list
         
+    def find_cars(self,model, color):  
+        list =[]
+        for car in self.__inventory:
+            if car.get_model()== model or model =='':
+                if car.get_color() == color or color == '':
+                    list.append(car) 
+        return list            
 
-
-
+    def ship_one_car(self, model, color):
+        shippedCar = None
+        for car in self.__inventory:
+            if car.get_model()== model and car.get_color() == color:
+                shippedCar = car
+                continue
+        if shippedCar != None:
+            self.__inventory.remove(shippedCar)  
+        else: 
+            print ("no such car in inventory") 
+                
+        
+                
 
 
 # def test():
