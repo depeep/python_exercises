5b4d75bfe66475537c8bf3c91c5a4d4acabea8cc
josvp <josvp@live.nl>
2026-04-19 22:10:33 +0200

    Changes to be committed: 	new file:   background.jpg 	new file:   background.png 	new file:   bark.wav 	new file:   multitreatsmall.png 	new file:   pygame/multitreat.png 	modified:   pygame/pygame_event1.py 	new file:   pygame/pygame_event2.py 	new file:   pygame/singletreat.png 	new file:   singletreatsmall.png

--- pygame/pygame_event1.py
+++ pygame/pygame_event1.py
@@ -5,9 +5,60 @@ screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Jos's event_demo1")
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-pygame.quit()
+corgi = pygame.image.load("corgi1.png")
+corgiX = 100
+corgiY = 100
+
+corgiMovingUp = False
+corgiMovingDown = False 
+corgiMovingRight = False
+corgiMovingLeft = False
+
+corgiSpeed = 5
+
+clock = pygame.time.Clock()
+while True:
+    clock.tick(60)
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        elif e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:    
+            pygame.quit()
+            exit()
+        elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_UP:
+                corgiMovingUp = True
+            elif e.key == pygame.K_DOWN:
+                corgiMovingDown = True
+            elif e.key ==  pygame.K_RIGHT:
+                corgiMovingRight = True
+            elif e.key == pygame.K_LEFT:
+                corgiMovingLeft = True  
+        
+        elif e.type == pygame.KEYUP:
+            if e.key == pygame.K_UP:
+                corgiMovingUp = False
+            elif e.key == pygame.K_DOWN:
+                corgiMovingDown = False
+            elif e.key ==  pygame.K_RIGHT:
+                corgiMovingRight = False
+            elif e.key == pygame.K_LEFT:
+                corgiMovingLeft = False
+
+    if corgiMovingUp:
+        corgiY -= corgiSpeed
+    if corgiMovingDown:
+        corgiY += corgiSpeed
+    if corgiMovingRight:
+        corgiX += corgiSpeed
+    if corgiMovingLeft:
+        corgiX -= corgiSpeed
+
+
+    screen.fill((255, 255, 255))
+    screen.blit(corgi, (corgiX, corgiY))    
+    pygame.display.update()
+
+
+
